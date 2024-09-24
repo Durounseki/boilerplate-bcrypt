b5262d2a505e314fea545fda1acdc6a5e286ad74
Christian Esparza <cesparzajp@gmail.com>
2024-09-24 16:33:47 +0900

    Initial commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt = require('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -11,12 +12,30 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword,saltRounds, (err,hash)=>{
+    if(err){
+        console.error(err);
+    }else{
+        console.log(hash);
+        bcrypt.compare(myPlaintextPassword,hash,(err,res)=>{
+            if(err){
+                console.error(err);
+            }else{
+                console.log(res);
+            }
+        });
+    }
+});
 
 //END_ASYNC
 
 //START_SYNC
 
+const hash = bcrypt.hashSync(myPlaintextPassword,saltRounds);
+console.log(hash);
+
+const result =  bcrypt.compareSync(myPlaintextPassword,hash);
+console.log(result);
 
 
 //END_SYNC
